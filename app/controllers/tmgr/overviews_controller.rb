2b225a592acedebe978341f161f19145cdeafc4d
mgiacomi <mgiacomi@gltech.com>
2016-07-18 23:51:54 -0700

    Can now add payments

--- app/controllers/tmgr/overviews_controller.rb
+++ app/controllers/tmgr/overviews_controller.rb
@@ -37,11 +37,27 @@ class Tmgr::OverviewsController < ApplicationController
 
   def view
     @registration = Registration.find(params[:id])
+    @payment = Payment.new
+    @payment.registration = @registration
   end
 
   def download_loi
     registration = Registration.find(params[:id])
     file_path = "#{Rails.configuration.loi_file_dir}/#{registration.id}#{registration.file_ext}"
     send_file file_path, disposition: 'attachment', filename: registration.file_url
+  end
+
+  def payment
+    @registration = Registration.find(params[:id])
+    @payment = Payment.new(params[:payment])
+    @payment.registration = @registration
+    @payment.user = current_user
+    @payment.receipt_date = Time.now
+
+    if @payment.save
+      redirect_to tmgr_form_view_path, notice: 'Payment has been added.'
+    else
+      render action: "view", notice: "Failed to add Payment."
+    end
   end
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,7 @@
 class Payment < ActiveRecord::Base
 
   belongs_to :registration
+  belongs_to :user
 
   validates :amount, numericality: true, allow_blank: true
 
