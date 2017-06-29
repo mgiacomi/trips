d702d9c0bb4163be0ebe3b7253e6dc38464cf2ef
mgiacomi <Rebeka13!>
2017-06-28 22:43:48 -0700

    Added helper for grade

--- app/helpers/register_helper.rb
+++ app/helpers/register_helper.rb
@@ -1,2 +1,17 @@
 module RegisterHelper
+
+  def textByGrade(grade)
+puts "grade: #{grade}"
+    case grade
+      when 5
+        "5th Grade"
+      when 8
+        "8th Grade"
+      when 11
+        "High School"
+      else
+        "Unknown grade #{grade}"
+    end
+  end
+
 end
