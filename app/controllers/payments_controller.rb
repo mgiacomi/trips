5a56364ef13b68db8cd05ef1f5c8d1ef1e76593c
mgiacomi <Rebeka13!>
2017-06-30 21:04:19 -0700

    Setup paypal test

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -16,16 +16,16 @@ class PaymentsController < ApplicationController
 
   def receipt
     if params[:payment_status] == "Completed"
-      parent_id = params[:invoice].split('-')[0]
-      parent = Parent.find parent_id
+      reg_id = params[:invoice].split('-')[0]
+      registration = Registration.find reg_id
 
       payment = Payment.new
       payment.amount = params[:payment_gross]
       payment.fee = params[:payment_fee]
       payment.pmtdate = params[:payment_date]
       payment.pmtnum = "Paypal"
-      payment.parent = parent
-      payment.user = parent.user
+      payment.registration = registration
+      payment.user = current_user
 
       if payment.save
         logger.info "Saved Payment: #{params[:invoice]} #{params[:payment_gross]} #{params[:payment_status]} #{params[:payment_date]}"
