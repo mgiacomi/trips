74be0d081c14837a4af82ebdc684b2f8bc7eafef
mgiacomi <Rebeka13!>
2017-07-02 12:55:45 -0700

    Fix Date format

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -10,6 +10,14 @@ class FormsController < ApplicationController
     @registration = Registration.where("user_id=? and id=?", current_user.id, params[:id]).first
     @loi = Loi.find_or_initialize_by(registration_id: @registration.id)
 
+    unless params[:loi]['p1sigdate'].blank?
+      params[:loi]['p1sigdate'] = Date.strptime(params[:loi]['p1sigdate'],"%m/%d/%Y")
+    end
+
+    unless params[:loi]['p2sigdate'].blank?
+      params[:loi]['p2sigdate'] = Date.strptime(params[:loi]['p2sigdate'],"%m/%d/%Y")
+    end
+
     if @loi.update_attributes(params[:loi])
       redirect_to todos_home_path, notice: 'LOI Signed successfully.'
     else
--- app/models/loi.rb
+++ app/models/loi.rb
@@ -5,7 +5,7 @@ class Loi < ActiveRecord::Base
 
   validates :p1name, :p1phone, :p1email, :p1address, :p1signature, :p1understand, :p1relationship, :p1sigdate, presence: {message: " is required"}
   validates :p2name, :p2phone, :p2email, :p2address, :p2signature, :p2understand, :p2relationship, :p2sigdate, presence: {message: " is required"}, :if => :signed_sig2
-  validates_date :p1sigdate, before: lambda {1.years.from_now}, after: lambda {1.years.ago}, before_message: "date is too recent", after_message: "date is too old"
+  validates_date :p1sigdate, before: lambda {2.days.from_now}, after: lambda {2.days.ago}, before_message: "date is too recent", after_message: "date is too old"
 
   # Needs to move to a helper
   def p1sigdate_formatted
