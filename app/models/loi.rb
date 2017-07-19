dc7e39c5f3c787beae12b05395390e0498a4ddee
mgiacomi <Rebeka13!>
2017-07-18 22:58:48 -0700

    Fix loi sign date issue.

--- app/models/loi.rb
+++ app/models/loi.rb
@@ -5,12 +5,18 @@ class Loi < ActiveRecord::Base
 
   validates :p1name, :p1phone, :p1email, :p1address, :p1signature, :p1understand, :p1relationship, :p1sigdate, presence: {message: " is required"}
   validates :p2name, :p2phone, :p2email, :p2address, :p2signature, :p2understand, :p2relationship, :p2sigdate, presence: {message: " is required"}, :if => :signed_sig2
-  validates_date :p1sigdate, before: lambda {2.days.from_now}, after: lambda {2.days.ago}, before_message: "date is too recent", after_message: "date is too old"
+  validates_date :p1sigdate, before: lambda {2.days.from_now}, after: lambda {1.years.ago}, before_message: "date is too recent", after_message: "date is too old"
 
   # Needs to move to a helper
   def p1sigdate_formatted
     unless p1sigdate.nil?
       p1sigdate.strftime("%m/%d/%Y")
+    end
+  end
+
+  def p2sigdate_formatted
+    unless p2sigdate.nil?
+      p2sigdate.strftime("%m/%d/%Y")
     end
   end
 
