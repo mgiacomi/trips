e70173d49387e26bba9d3fb201a9744b65351321
mgiacomi <Rebeka13!>
2017-12-04 23:42:22 -0800

    Added SSI Admin support.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-#password = SecureRandom.urlsafe_base64(6)
-#user = User.create(email: 'mgiacomi@gltech.com', password: password, admin: true)
-#puts "#{user.email} #{password}"
-#
-#password = SecureRandom.urlsafe_base64(6)
-#user = User.create(email: 'kblack@pdx.edu', password: password, admin: true)
-#puts "#{user.email} #{password}"
-#
-#password = SecureRandom.urlsafe_base64(6)
-#user = User.create(email: 'walcottma@msn.com', password: password, admin: true)
-#puts "#{user.email} #{password}"
+password = SecureRandom.urlsafe_base64(6)
+user = User.create(email: 'mgiacomi@gltech.com', password: password, admin: true)
+puts "#{user.email} #{password}"
+
+password = SecureRandom.urlsafe_base64(6)
+user = User.create(email: 'kblack@pdx.edu', password: password, admin: true)
+puts "#{user.email} #{password}"
+
+password = SecureRandom.urlsafe_base64(6)
+user = User.create(email: 'walcottma@msn.com', password: password, admin: true)
+puts "#{user.email} #{password}"
 
 password = SecureRandom.urlsafe_base64(6)
 user = User.create(email: 'ssi.ishibuchi@gmail.com', password: password, ssi_admin: true)
