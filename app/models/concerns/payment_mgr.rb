9d0f8db2dfb5b958a273f1ce8d4a7b886148d396
mgiacomi <mgiacomi@gltech.com>
2022-10-14 09:56:07 -0700

    Updates for 2022-2023

--- app/models/concerns/payment_mgr.rb
+++ app/models/concerns/payment_mgr.rb
@@ -4,7 +4,7 @@ module PaymentMgr
   module ClassMethods
     def get_5th_pay_schedule
       records = Array.new
-      records << {date: Date.strptime("10/1/2022", '%m/%d/%Y'), student: 0, chaperone: 0}
+      records << {date: Date.strptime("11/1/2022", '%m/%d/%Y'), student: 200, chaperone: 0}
       records << {date: Date.strptime("11/15/2022", '%m/%d/%Y'), student: 600, chaperone: 0}
       records << {date: Date.strptime("12/15/2022", '%m/%d/%Y'), student: 600, chaperone: 700}
       records << {date: Date.strptime("1/15/2023", '%m/%d/%Y'), student: 600, chaperone: 700}
