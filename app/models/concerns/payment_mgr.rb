297603ca0e1cb2d17e4be9e105ff712443b4f5fe
mgiacomi <mgiacomi@gltech.com>
2023-02-27 21:50:38 -0800

    Enable SSI

--- app/models/concerns/payment_mgr.rb
+++ app/models/concerns/payment_mgr.rb
@@ -25,11 +25,12 @@ module PaymentMgr
 
     def get_ssi_pay_schedule
       records = Array.new
-      records << {date: Date.strptime("2/10/2023", '%m/%d/%Y'), student: 800, chaperone: 0}
-      records << {date: Date.strptime("2/28/2023", '%m/%d/%Y'), student: 800, chaperone: 0}
-      records << {date: Date.strptime("3/31/2023", '%m/%d/%Y'), student: 800, chaperone: 0}
-      records << {date: Date.strptime("4/30/2023", '%m/%d/%Y'), student: 800, chaperone: 0}
-      records << {date: Date.strptime("5/31/2023", '%m/%d/%Y'), student: 800, chaperone: 0}
+      records << {date: Date.strptime("2/24/2023", '%m/%d/%Y'), student: 800, chaperone: 0}
+      records << {date: Date.strptime("3/17/2023", '%m/%d/%Y'), student: 800, chaperone: 0}
+      records << {date: Date.strptime("4/14/2023", '%m/%d/%Y'), student: 800, chaperone: 0}
+      records << {date: Date.strptime("5/5/2023", '%m/%d/%Y'), student: 800, chaperone: 0}
+      records << {date: Date.strptime("5/26/2023", '%m/%d/%Y'), student: 800, chaperone: 0}
+      records << {date: Date.strptime("6/14/2023", '%m/%d/%Y'), student: 400, chaperone: 0}
     end
   end
 
